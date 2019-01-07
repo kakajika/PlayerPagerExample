1472487f00e459b0ad67b5951aa65705e685d83b
kakajika <kakajika200x@gmail.com>
2019-01-07 21:17:20 +0900

    Add Fragment for example of multi player in pager

---
+++ app/src/main/java/com/labo/kaji/aacexample/MultiPlayerPagerScrollHandler.kt
@@ -0,0 +1,33 @@
+package com.labo.kaji.aacexample
+
+import androidx.core.view.children
+import androidx.recyclerview.widget.RecyclerView
+import com.labo.kaji.aacexample.ext.muted
+
+abstract class MultiPlayerPagerScrollHandler(
+    private val pager: RecyclerView
+) : PagerScrollHandler(pager) {
+
+    override fun playVideoAtPage(position: Int) {
+        PlayerProvider.mutePlayer()
+
+        pager.children
+            .mapNotNull { pager.getChildViewHolder(it) }
+            .filterIsInstance<ExampleViewHolder>()
+            .onEach { it.playerView.player = null }
+            .forEach {
+                when (it.adapterPosition) {
+                    position -> {
+                        it.playerView.player = PlayerProvider
+                            .getPlayer(it.adapterPosition)
+                            .apply { muted = false }
+                    }
+                    position - 1, position + 1 -> {
+                        it.playerView.player = PlayerProvider
+                            .getPlayer(it.adapterPosition)
+                            .apply { muted = true }
+                    }
+                }
+            }
+    }
+}
