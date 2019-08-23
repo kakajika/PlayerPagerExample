22ff551ebb15b1f6b8c352142d7a916973418ea3
kakajika <kakajika200x@gmail.com>
2019-08-23 12:39:28 +0900

    Experiment for limiting scroll sensitivity

--- app/src/main/java/com/labo/kaji/aacexample/widget/LoopPagerRecyclerView.kt
+++ app/src/main/java/com/labo/kaji/aacexample/widget/LoopPagerRecyclerView.kt
@@ -2,8 +2,11 @@ package com.labo.kaji.aacexample.widget
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.GestureDetector
+import android.view.MotionEvent
 import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.abs
 
 class LoopPagerRecyclerView : LoopRecyclerView {
 
@@ -18,12 +21,26 @@ class LoopPagerRecyclerView : LoopRecyclerView {
         it.attachToRecyclerView(this)
     }
 
+    private val gestureDetector by lazy {
+        GestureDetector(context, XScrollDetector())
+    }
+
     override fun getCurrentPosition(): Int {
         val snapView = snapHelper.findSnapView(layoutManager)
         return if (snapView != null) {
             layoutManager!!.getPosition(snapView)
         } else {
             RecyclerView.NO_POSITION
+        }
+    }
+
+    override fun onInterceptTouchEvent(e: MotionEvent?): Boolean {
+        return super.onInterceptTouchEvent(e) and gestureDetector.onTouchEvent(e)
+    }
+
+    class XScrollDetector : GestureDetector.SimpleOnGestureListener() {
+        override fun onScroll(e1: MotionEvent, e2: MotionEvent, distanceX: Float, distanceY: Float): Boolean {
+            return abs(distanceY) < abs(distanceX)
         }
     }
 }
